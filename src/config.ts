1f17c1d9825a72d3d3f4c2131aba0d3aa3d8dee8
kd64i <123563229+kd64i@users.noreply.github.com>
2026-02-07 00:40:27 +0800

    Upload config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export default {
   // 网站描述
   Description: '惊鲵的博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
   // 网站作者
-  Author: '惊鲵',
+  Author: '惊鲵2',
   // 作者头像
   Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=838332985&s=640',
   // 网站座右铭
